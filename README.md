e0eecc73e517839bb44a9b38a2c0a1860fdf02f3
Asher <ashersilb@gmail.com>
2017-06-18 00:19:20 -0700

    Section 2.1

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 A way to catch up on or relisten to podcasts
 
 What am I doing?
-[] Working through udemy vuejs course
+[] Working through udemy vuejs course: Section 2
 [] Flask tutorial http://maximebf.com/blog/2012/10/building-websites-in-python-with-flask/
+
+TODO:
+Add favicon
--- Recaster.py
+++ Recaster.py
@@ -2,6 +2,18 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+class CustomFlask(Flask):
+    jinja_options = Flask.jinja_options.copy()
+    jinja_options.update(dict(
+        block_start_string='$$',
+        block_end_string='$$',
+        variable_start_string='$',
+        variable_end_string='$',
+        comment_start_string='$#',
+        comment_end_string='#$',
+    ))
+
+app = CustomFlask(__name__)
 
 @app.route('/')
 def index():
