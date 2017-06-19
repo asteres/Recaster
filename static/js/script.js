9aba7488b3601fdc10af9abe5526d086d53e74d3
Asher <ashersilb@gmail.com>
2017-06-18 17:11:57 -0700

    fixed! I had a div in the wrong place.

---
+++ static/js/script.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Hanweir on 6/17/2017.
+ */
+
+new Vue({
+    el: '#app',
+    data: {
+        title: 'Hello World!'
+        },
+    methods: {
+        changeTitle: function(event) {
+            this.title = event.target.value;
+        }
+    }
+});
+
