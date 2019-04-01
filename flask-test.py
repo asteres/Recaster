24fb92db6688db45f60c0ea975c81ce1fae25af9
Asher <asherehsa@gmail.com>
2019-03-31 17:44:51 -0700

    added flask-restful example

---
+++ flask-test.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        some_json = request.get_json()
+        return jsonify({'you sent': some_json}), 201
+    else:
+        return jsonify({"Header": "Hello World!"})
+
+
+# /multi returns the number multiplied by 10
+@app.route('/multi/<int:num>', methods=['GET'])
+def get_multiply10(num):
+    return jsonify({'result': num * 10})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
